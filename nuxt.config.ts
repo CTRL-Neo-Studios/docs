d0059cd72ce83b80d8bba114e9ce446bac6a63db
Type-32 <87076491+Type-32@users.noreply.github.com>
2025-11-03 21:30:27 +0800

    chore: update tauri-sqlite-orm docs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,19 @@
 export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/i18n',
-    '@nuxt/content',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/ui',
-  ],
-  i18n: {
-    defaultLocale: 'en',
-    locales: [{
-      code: 'en',
-      name: 'English',
-    }, {
-      code: 'fr',
-      name: 'Français',
-    }],
-  },
+    modules: [
+        '@nuxtjs/i18n',
+        '@nuxt/content',
+        '@nuxt/image',
+        '@nuxt/scripts',
+        '@nuxt/ui',
+    ],
+    i18n: {
+        defaultLocale: 'en',
+        locales: [{
+            code: 'en',
+            name: 'English',
+        }],
+    },
+    site: {
+        name: 'CTRL Docs'
+    }
 })
